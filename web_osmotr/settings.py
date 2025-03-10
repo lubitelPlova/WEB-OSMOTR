d9c1677e3a3772638ca4d0f7e38ebfe8d58f5e56
SamuraiPetya <mazur.dmitry03@gmail.com>
2025-03-10 21:06:03 +0700

    Done 4 lab

--- web_osmotr/settings.py
+++ web_osmotr/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
+import environ
 from pathlib import Path
 
 from django.conf.global_settings import STATICFILES_DIRS
@@ -22,6 +23,23 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-08lak!n+y_(_siitsij)r$hkgb=^e%nj7+b1&^^ksh&dlgeo=$'
+
+env = environ.Env(
+DEBUG=(bool, False)
+)
+environ.Env.read_env()
+DEBUG = env('DEBUG')
+SECRET_KEY = env('SECRET_KEY')
+DATABASES = {
+'default': env.db(),
+}
+6
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = env('EMAIL_PORT')
+EMAIL_USE_TLS = True
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
