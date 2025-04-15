6c2d9cad46998c5b4776b6b1eb0895e88a6f6ccd
SamuraiPetya <mazur.dmitry03@gmail.com>
2025-04-16 00:45:44 +0700

    Done 9 lab

--- shop/urls.py
+++ shop/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-
 # from web_osmotr.urls import urlpatterns
-from .views import HomePageView
+from .views import HomePageView, DoctorsListView, PatientsListView
 
 urlpatterns = [
-    path('',HomePageView.as_view(), name = 'home')
+    path('',HomePageView.as_view(), name = 'home'),
+    path('patients', PatientsListView.as_view(), name='patients'),
+    path('doctors', DoctorsListView.as_view(), name='doctors')
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-
-
+from django.views.generic import ListView
+from shop.models import Patient, Doctor
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+class PatientsListView(ListView):
+    template_name = 'patients.html'
+    model = Patient
+    context_object_name = 'list_of_all_patients'
+
+class DoctorsListView(ListView):
+    template_name = 'doctor.html'
+    model = Doctor
+    context_object_name = 'list_of_all_doctors'
